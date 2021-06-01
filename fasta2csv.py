7686303305df7381e5a261dec7469849270c3e80
kaynzhel <crayzhel_misay@yahoo.com>
2021-06-01 13:53:14 -0600

    Update fasta2csv.py

--- fasta2csv.py
+++ fasta2csv.py
@@ -1,9 +1,9 @@
 '''
 Author: Kaye Ena Crayzhel F. Misay
 Date Created: May 14, 2021
-Date Last Edited: May 20, 2021
+Date Last Edited: June 01, 2021
 
-This code only prints out the Accession ID and the Sequence from the fasta file.
+This code only prints out the Accession ID, Accession Description, and the Sequence from the fasta file.
 From here, you can change the code to accommodate with your needs.
 
 References:
@@ -16,17 +16,18 @@ import csv, argparse
 def main():
     file_name = getFileName()
     # transforms the fasta file to a csv file with headers accession and its sequence
-    with open('converted_fasta_file.csv', mode='w') as csv_file:
-        field_names = ['Accession', 'Sequence'] # header
+    dot_index = file_name.index('.')
+    with open(file_name[:dot_index] + '.csv', mode='w') as csv_file:
+        field_names = ['Accession ID', 'Accession Description', 'Sequence'] # header
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
         writer.writeheader()
         for seq_record in SeqIO.parse(file_name, "fasta"): # parser
-            row = {'Accession': seq_record.id, 'Sequence': seq_record.seq}
+            row = {'Accession Description': seq_record.id, 'Accession Description': seq_record.description, 'Sequence': seq_record.seq}
             writer.writerow(row)
 
 def getFileName():
     # command line argument
-    description = 'Give the file name of the fasta file to be converted.'
+    description = 'Give the file name of the converted csv file.'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("file_name", type=str)
     args = parser.parse_args()
