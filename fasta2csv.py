c862b508aa0fc7c13e32aee34aaf36f4f65d9177
kaynzhel <crayzhel_misay@yahoo.com>
2021-06-01 14:00:32 -0600

    Update fasta2csv.py
    Added better output file name

--- fasta2csv.py
+++ fasta2csv.py
@@ -3,7 +3,7 @@ Author: Kaye Ena Crayzhel F. Misay
 Date Created: May 14, 2021
 Date Last Edited: June 01, 2021
 
-This code only prints out the Accession ID, Accession Description, and the Sequence from the fasta file.
+This code only prints out the Accession ID and the Sequence from the fasta file.
 From here, you can change the code to accommodate with your needs.
 
 References:
@@ -18,11 +18,11 @@ def main():
     # transforms the fasta file to a csv file with headers accession and its sequence
     dot_index = file_name.index('.')
     with open(file_name[:dot_index] + '.csv', mode='w') as csv_file:
-        field_names = ['Accession ID', 'Accession Description', 'Sequence'] # header
+        field_names = ['Accession ID', 'Sequence'] # header
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
         writer.writeheader()
         for seq_record in SeqIO.parse(file_name, "fasta"): # parser
-            row = {'Accession Description': seq_record.id, 'Accession Description': seq_record.description, 'Sequence': seq_record.seq}
+            row = {'Accession ID': seq_record.id, 'Sequence': seq_record.seq}
             writer.writerow(row)
 
 def getFileName():
